bcf10af47acce5a6123b24cd687f258f38985669
DESKTOP-DCFI7BM\Aarti zinzuvadiya <aarti.zinzuvadiya@appguruz.com>
2024-02-12 21:49:47 +0530

    [Added] New Features Into TextForm Component

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 export default function TextForm(props) {
 
-    const [text, setText] = useState("Enter The Text...");
+    const [text, setText] = useState("Enter the text...");
 
     const HandleOnChange = (event) => {
         console.log("OnChange");
@@ -11,13 +11,25 @@ export default function TextForm(props) {
     };
 
     const upperCase = () => {
-        console.log("OnClick");
         const toUpperCase = text.toUpperCase();
         setText(toUpperCase);
     };
 
+    const titleCase = () => {
+        let newText = text.split(" ").map(word => CapitalizeFirstLetter(word)).join(" ");
+        setText(newText);
+    };
+
+    const clearText = () => {
+        const newText = "";
+        setText(newText);
+    }
+
+    const CapitalizeFirstLetter = (word) => {
+        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    }
+
     const lowerCase = () => {
-        console.log("OnClick");
         const toUpperCase = text.toLowerCase();
         setText(toUpperCase);
     };
@@ -28,7 +40,9 @@ export default function TextForm(props) {
                 <h1>{props.heading}</h1>
                 <textarea id="my-box" className="form-control" onChange={HandleOnChange} value={text} rows="5"></textarea><br />
                 <button className="btn btn-primary mx-3" onClick={upperCase}>Convert To UpperCase</button>
-                <button className="btn btn-primary" onClick={lowerCase}>Convert To LowerCase</button>
+                <button className="btn btn-primary mx-3" onClick={lowerCase}>Convert To LowerCase</button>
+                <button className="btn btn-primary mx-3" onClick={titleCase}>Convert To TitleCase</button>
+                <button className="btn btn-primary mx-3" onClick={clearText}>Clear Text</button>
             </div>
             <div className="container">
                 <h2>Your Text Summary </h2>
@@ -43,5 +57,5 @@ export default function TextForm(props) {
 }
 
 TextForm.propTypes = {
-    heading: PropTypes.string,
+    heading: PropTypes.string
 }
